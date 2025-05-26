86a4a8a60eca788765aa05420c48af32a0bd2a51
inha1234 <wind150804@gmail.com>
2025-05-26 21:24:34 +0900

    댓글 작성 테스트 케이스 리팩토링(스태틱), 대댓글 작성 테스트 케이스 작성(미완성)

--- src/main/java/com/springboard/entity/Comment.java
+++ src/main/java/com/springboard/entity/Comment.java
@@ -1,6 +1,7 @@
 package com.springboard.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
--- src/test/java/com/springboard/service/CommentServiceTest.java
+++ src/test/java/com/springboard/service/CommentServiceTest.java
@@ -8,6 +8,7 @@ import com.springboard.entity.User;
 import com.springboard.repository.CommentRepository;
 import com.springboard.repository.PostRepository;
 import com.springboard.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -32,33 +33,60 @@ public class CommentServiceTest {
     @Mock
     private UserRepository userRepository;
 
-    @Test
-    void 댓글_작성_성공_일반_댓글(){
-        User user = new User();
+    static User user;
+    static Post post;
+    static Comment comment;
+    @BeforeAll
+    static void beforeAll(){
+        user = new User();
         user.setUsername("testUser");
         user.setNickname("testUser");
         user.setPassword("1234");
 
-
-        Post post = new Post();
+        post = new Post();
         post.setTitle("테스트 제목");
         post.setContent("테스트 내용");
         post.setUser(user);
 
+        comment = new Comment();
+        comment.setContent("테스트 댓글");
+        comment.setPost(post);
+        comment.setUser(user);
+    }
+    @Test
+    void 댓글_작성_성공_일반_댓글(){
         CommentRequestDto dto = new CommentRequestDto();
         dto.setContent("댓글 내용");
 
         given(postRepository.findById(1L)).willReturn(Optional.of(post));
         given(userRepository.findByUsername(anyString())).willReturn(Optional.of(user));
-        given(commentRepository.save(any(Comment.class))).willAnswer(invocation -> {
-            Comment saved = invocation.getArgument(0);
-            return saved;
-        });
+//        given(commentRepository.save(any(Comment.class))).willAnswer(invocation -> {
+//            Comment saved = invocation.getArgument(0);
+//            return saved;
+//        }); //필요없을 거 같으니깐 주석처리
 
         CommentResponseDto responseDto = commentService.createComment(1L, dto, user.getUsername());
 
         assertThat(responseDto.getContent()).isEqualTo("댓글 내용");
         assertThat(responseDto.getParentId()).isNull();
+    }
+
+    @Test
+    void 댓글_작성_성공_대댓글(){
+        Long parentCommentId = comment.getId();
+
+        CommentRequestDto dto = new CommentRequestDto();
+        dto.setContent("대댓글 내용");
+        dto.setParentId(parentCommentId);
+
+        given(postRepository.findById(1L)).willReturn(Optional.of(post));
+        given(userRepository.findByUsername(anyString())).willReturn(Optional.of(user));
+        given(commentRepository.findById(parentCommentId)).willReturn(Optional.of(comment));
+
+        CommentResponseDto responseDto = commentService.createComment(1L, dto, user.getUsername());
+
+        assertThat(responseDto.getContent()).isEqualTo("대댓글 내용");
+        assertThat(responseDto.getParentId()).isEqualTo(parentCommentId);
     }
 
 }
