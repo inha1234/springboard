664eb9a00c57d4e8714f913b4fdcc36158df84d5
inha1234 <wind150804@gmail.com>
2025-08-08 19:57:48 +0900

    MySQL 연결 후 기능 테스트

--- src/main/java/com/springboard/config/SwaggerConfig.java
+++ src/main/java/com/springboard/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("local")
+//@Profile("local")
 public class SwaggerConfig {
 
     @Bean
